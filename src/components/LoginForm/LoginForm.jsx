b2b79ed1099940d4da0d5b3b9a1a4e08643858f9
Albina Vivdenko <albinka3322@gmail.com>
2024-10-17 17:03:58 +0200

    1

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,20 +1,22 @@
 import { useDispatch } from "react-redux";
 import { logIn } from "../../redux/auth/operations";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { Button, TextField, Box, Typography } from "@mui/material";
 import css from "./LoginForm.module.css";
+
+const validationSchema = Yup.object({
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Required"),
+});
 
 export const LoginForm = () => {
   const dispatch = useDispatch();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const form = e.currentTarget;
-
-    dispatch(
-      logIn({
-        email: form.elements.email.value,
-        password: form.elements.password.value,
-      })
-    )
+  const handleSubmit = (values, { resetForm }) => {
+    dispatch(logIn(values))
       .unwrap()
       .then(() => {
         console.log("login success");
@@ -22,31 +24,73 @@ export const LoginForm = () => {
       .catch(() => {
         console.log("login error");
       });
-
-    form.reset();
+    resetForm();
   };
 
   return (
-    <form
-      className={css.form}
-      onSubmit={handleSubmit}
-      autoComplete="off"
-    >
-      <label className={css.label}>
-        Email
-        <input
-          type="email"
-          name="email"
-        />
-      </label>
-      <label className={css.label}>
-        Password
-        <input
-          type="password"
-          name="password"
-        />
-      </label>
-      <button type="submit">Log In</button>
-    </form>
+    <Box className={css.container}>
+      <Typography
+        variant="h5"
+        gutterBottom
+        className={css.title}
+      >
+        Log In
+      </Typography>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form className={css.form}>
+            <Box className={css.field}>
+              <Field
+                name="email"
+                as={TextField}
+                label="Email"
+                fullWidth
+                variant="outlined"
+                className={css.input}
+                helperText={
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className={css.error}
+                  />
+                }
+              />
+            </Box>
+            <Box className={css.field}>
+              <Field
+                name="password"
+                as={TextField}
+                label="Password"
+                type="password"
+                fullWidth
+                variant="outlined"
+                className={css.input}
+                helperText={
+                  <ErrorMessage
+                    name="password"
+                    component="div"
+                    className={css.error}
+                  />
+                }
+              />
+            </Box>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth
+              disabled={isSubmitting}
+              className={css.button}
+            >
+              Log In
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Box>
   );
 };
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,53 +1,107 @@
 import { useDispatch } from "react-redux";
 import { register } from "../../redux/auth/operations";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { Button, TextField, Box, Typography } from "@mui/material";
 import css from "./RegisterForm.module.css";
+
+const validationSchema = Yup.object({
+  name: Yup.string().min(3, "Too Short!").required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Required"),
+});
 
 export const RegisterForm = () => {
   const dispatch = useDispatch();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const form = e.currentTarget;
-
-    dispatch(
-      register({
-        name: form.elements.name.value,
-        email: form.elements.email.value,
-        password: form.elements.password.value,
-      })
-    );
-
-    form.reset();
+  const handleSubmit = (values, { resetForm }) => {
+    dispatch(register(values));
+    resetForm();
   };
 
   return (
-    <form
-      className={css.form}
-      onSubmit={handleSubmit}
-      autoComplete="off"
-    >
-      <label className={css.label}>
-        Username
-        <input
-          type="text"
-          name="name"
-        />
-      </label>
-      <label className={css.label}>
-        Email
-        <input
-          type="email"
-          name="email"
-        />
-      </label>
-      <label className={css.label}>
-        Password
-        <input
-          type="password"
-          name="password"
-        />
-      </label>
-      <button type="submit">Register</button>
-    </form>
+    <Box className={css.container}>
+      <Typography
+        variant="h5"
+        gutterBottom
+        className={css.title}
+      >
+        Register
+      </Typography>
+      <Formik
+        initialValues={{ name: "", email: "", password: "" }}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form className={css.form}>
+            <Box className={css.field}>
+              <Field
+                name="name"
+                as={TextField}
+                label="Username"
+                fullWidth
+                variant="outlined"
+                className={css.input}
+                helperText={
+                  <ErrorMessage
+                    name="name"
+                    component="div"
+                    className={css.error}
+                  />
+                }
+              />
+            </Box>
+            <Box className={css.field}>
+              <Field
+                name="email"
+                as={TextField}
+                label="Email"
+                fullWidth
+                variant="outlined"
+                className={css.input}
+                helperText={
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className={css.error}
+                  />
+                }
+              />
+            </Box>
+            <Box className={css.field}>
+              <Field
+                name="password"
+                as={TextField}
+                label="Password"
+                type="password"
+                fullWidth
+                variant="outlined"
+                className={css.input}
+                helperText={
+                  <ErrorMessage
+                    name="password"
+                    component="div"
+                    className={css.error}
+                  />
+                }
+              />
+            </Box>
+            <Button
+              className={css.button}
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth
+              disabled={isSubmitting}
+            >
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Box>
   );
 };
